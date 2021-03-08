83277f2321f10ebb6b50d0e4434b15254b8e7f98
Rishiraj0100 <73101003+Rishiraj0100@users.noreply.github.com>
2021-03-08 09:06:27 +0530

    Update cog.py

--- discoutils/cog.py
+++ discoutils/cog.py
@@ -1,2 +1,31 @@
-import discord
+import discord, json, random
 from discord.ext import commands
+
+class CMDs(command.Cog, name="discoCMDs"):
+  def __init__(self, bot):
+    self.bot = bot
+  
+  @commands.command()
+  async def kill(self, ctx, user: discord.Member):
+    """used to get some random funny kill message
+    parameters
+      • user
+        use id or mention a user to get message
+    """
+    with open("discoutils/assets/kill.json", "r") as f:
+      kill = json.load(f)
+      
+    kills = kill["kills"]
+    
+    response = random.choice(kills)
+    
+    if "$author" in response:
+      response=response.replace("$author", ctx.author.name)
+      
+    if "$mention" in response:
+      response = response.replace("$mention", user.name)
+      
+    await ctx.send(response).add_reaction("🤣).add_reaction("😂")
+    
+def setup(bot: commands.Bot):
+  bot.add_cog(CMDs)              

64364b56e77f07b521f4af43ea12efb8e3ea9b71
Rishidev <73101003+Rishiraj0100@users.noreply.github.com>
2021-08-10 09:50:33 +0530

    Update __init__.py

--- discoutils/cogs/__init__.py
+++ discoutils/cogs/__init__.py
@@ -26,14 +26,21 @@ class BaseCog(commands.Cog):
 
     if isinstance(error, ignored):
       return
+
     if isinstance(error, commands.DisabledCommand):
-      await ctx.send(f'{ctx.command} has been disabled.')
+      return await ctx.reply(f'This command has been disabled.')
     elif isinstance(error, commands.NoPrivateMessage):
       try:
-        await ctx.author.send(
-            f'{ctx.command} can not be used in DMs.')
+        return await ctx.author.send(
+            f'`{ctx.prefix}{ctx.command}` can not be used in DMs.')
       except discord.HTTPException:
         pass
+    elif isinstance(error, commands.BotMissingPermissions):
+      fmt = self.get_missing_perms(error)
+      return await ctx.reply(content=f"I don't have {fmt} permission(s) to run this command.")
+    elif isinstance(error, commands.MissingPermissions):
+      fmt = self.get_missing_perms(error)
+      return await ctx.reply(content=f"You don't have {fmt} permission(s) to run this command.")
     else:
       print('Ignoring exception in command {}:'.format(ctx.command),
             file=sys.stderr)
@@ -43,4 +50,16 @@ class BaseCog(commands.Cog):
                                 file=sys.stderr)
 
 
-
+  def get_missing_perms(self, exc):
+    missing_permissions = exc.missing_permissions
+    missing = [
+        perm.replace('_', ' ').replace('guild', 'server').title()
+        for perm in missing_permissions
+    ]
+    if len(missing) > 2:
+      fmt = '{}, and {}'.format(", ".join(missing[:-1]), missing[-1])
+    else:
+      fmt = ' and '.join(missing)
+    #message = f"I don't have {fmt} permission(s) to run this command."
+    return fmt
+  

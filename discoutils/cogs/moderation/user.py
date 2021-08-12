56d8176275b733fd7526d6226ce49d82410d67aa
Rishidev <73101003+Rishiraj0100@users.noreply.github.com>
2021-08-12 11:56:19 +0530

    Update user.py

--- discoutils/cogs/moderation/user.py
+++ discoutils/cogs/moderation/user.py
@@ -100,5 +100,60 @@ class usermod(BaseCog, name="moderation"):
       
     await ctx.send(f"Successfully unbanned {unbanned_user}")
 
+  @commands.group(name="role", invoke_without_command=True)
+  async def role(self, ctx):
+    e = discord.Embed(title="Role Commands")
+    e.add_field(name=f"{ctx.prefix}{ctx.command} add `@user @role`, value="Adds role to the given user")
+    e.add_field(name=f"{ctx.prefix}{ctx.command} remove `@user @role`, value="Remove role from the given user")
+    await ctx.send(embed=e)
+
+  @role.command(name="add')
+  @commands.guild_only()
+  @commands.has_permissions(manage_roles=True)
+  @commands.bot_has_permissions(manage_roles=True)
+  async def role_add(self, ctx, member: discord.Member, *,role: discord.Role):
+    """Add a role to someone else
+
+        Parameters
+        ------------
+        • member - the user to whom role is to be added
+        • role - the role which is to be added
+        """
+    reason = f"Action done by {ctx.author.name}#{ctx.author.discriminator}"
+    if role in member.roles:
+      return await ctx.send(f"{member.mention} already have this role")
+    
+    try:
+      await member.add_roles(role, reason=reason)
+    except:
+      await ctx.send("I can't add role to  {member.mention} due to discord role hiearchy")
+    else:
+      await ctx.send(f"Successfully added the role `{role.name}` to {member.mention}")
+  
+
+  @role.command(name="remove")
+  @commands.guild_only()
+  @commands.has_permissions(manage_roles=True)
+  @commands.bot_has_permissions(manage_roles=True)
+  async def role_remove(self, ctx, member: discord.Member, *,role: discord.Role):
+    """Remove a role from someone else
+
+        Parameters
+        ------------
+        • member - the user from whom the role is to be taken
+        • role - the role which is to be removed
+        """
+    reason = f"Action done by {ctx.author.name}#{ctx.author.discriminator}"
+    if role not in member.roles:
+      return await ctx.send(f"{member.mention} doesn't have this role")
+    
+    try:
+      await member.remove_roles(role, reason=reason)
+    except:
+      await ctx.send("I can't remove roles from {member.mention} due to discord role hiearchy")
+    else:
+      await ctx.send(f"Successfully removed the role `{role.name}` from {member.mention}")
+    
+
 def setup(bot):
   bot.add_cog(usermod(bot))

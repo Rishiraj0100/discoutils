c0aaa0165a1f7609d9a8a39aaf86582dc8c00e59
Rishidev <73101003+Rishiraj0100@users.noreply.github.com>
2021-08-12 18:16:49 +0530

    Create kill.py

---
+++ discoutils/cogs/fun/kill.py
@@ -0,0 +1,31 @@
+import discord, requests
+from discord.ext import commands
+from discoutils.cogs import BaseCog
+
+class Kill(BaseCog, name="fun"):
+  "Fun commands"
+
+  @commands.command()
+  async def kill(self, ctx, user: discord.Member):
+    """used to get some random funny kill message
+
+    Parameters
+    -------------
+      • user - the user to whom you wanna kill
+    """
+    kill = requests.get("https://api.rrrdev.cf/api/kill").json()
+    if user.id == ctx.author.id:
+      return await ctx.send("Why do u want to kill yourself??")
+      
+    response = kill.get("args")
+    if "$author" in response:
+      response=response.replace("$author", ctx.author.name)
+      
+    if "$mention" in response:
+      response = response.replace("$mention", user.name)
+      
+    await ctx.send(response)
+
+
+def setup(bot):
+  bot.add_cog(Kill(bot))

4b70dd37a9d7ccf00644e9aaa5b1fcff384d1a81
Rishidev <73101003+Rishiraj0100@users.noreply.github.com>
2021-08-12 18:36:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 from setuptools import setup, find_packages
+import re
 
 with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
+version = ''
+with open('discoutils/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if version.endswith(('a', 'b', 'rc')):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+g' + out.decode('utf-8').strip()
+    except Exception:
+        pass
 
 setup(
     name="discoutils",
-    version="0.0.3.3",
+    version=version,
     description="Some utilities for discord.py. Making Discord bot development easier.",
     long_description=readme,
     long_description_content_type="text/markdown",

a9ee89bf5593d534e9a3dc5e7dafe99260bb64cd
Rishidev <73101003+Rishiraj0100@users.noreply.github.com>
2021-08-14 10:47:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "Programming Language :: Python :: 3.8",
     ],
     python_requires=">=3.8",
-    install_requires=["discord.py >=1"],
+    install_requires=["git+https://github.com/Rapptz/discord.py@master"],
     keywords="discord discord-py discord-bot utils utility",
     packages=find_packages(exclude=["docs", "tests"]),
     data_files=None,
